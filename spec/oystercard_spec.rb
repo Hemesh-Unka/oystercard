afd7394ebc85432e62e4b60277617b49a3edc3cc
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-28 13:44:38 +0000

    Closing #5: added top_up method & asscociated tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,14 @@ describe Oystercard do
 
   describe 'balance' do
     it 'expects a new instance of Oystercard class to have a balance of 0' do
-      expect(subject.balance).to eq(0)
+      expect(oystercard.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it 'should update the balance with the specified top up amount' do
+      oystercard.top_up(1)
+      expect(oystercard.balance).to eq(1)
     end
   end
 end
