1b11add489b67e219a43c29a12c528bdcf7b19cd
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-28 13:36:31 +0000

    Closing #3: added balance & associated tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  subject(:oystercard) { described_class.new }
+
+  describe 'balance' do
+    it 'expects a new instance of Oystercard class to have a balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
